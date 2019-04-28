66adcf8d372abe29cf8243d1b714055ae498702b
Mathew Varughese <mat.varughese3@gmail.com>
2019-04-28 14:45:13 -0400

    give it a background and a logo

--- app.js
+++ app.js
@@ -12,14 +12,28 @@ function transformToD3Tree(teams) {
 				{
 					"name": "C1",
 					"children": [
-					],
-					"value": 1
+						{ "name": "D1", "children": [
+							{ "name": "E1"},
+							{ "name": "E2"}
+						] },
+						{ "name": "D2", "children": [
+							{ "name": "E1"},
+							{ "name": "E2"}
+						] }
+					]
 				},
 				{
 					"name": "C1",
 					"children": [
-					],
-					"value": 1
+						{ "name": "D1", "children": [
+							{ "name": "E1"},
+							{ "name": "E2"}
+						] },
+						{ "name": "D2", "children": [
+							{ "name": "E1"},
+							{ "name": "E2"}
+						] }
+					]
 				},
 			]
 		  },
@@ -29,14 +43,28 @@ function transformToD3Tree(teams) {
 				{
 					"name": "C1",
 					"children": [
-					],
-					"value": 1
+						{ "name": "D1", "children": [
+							{ "name": "E1"},
+							{ "name": "E2"}
+						] },
+						{ "name": "D2", "children": [
+							{ "name": "E1"},
+							{ "name": "E2"}
+						] }
+					]
 				},
 				{
 					"name": "C1",
 					"children": [
-					],
-					"value": 1
+						{ "name": "D1", "children": [
+							{ "name": "E1"},
+							{ "name": "E2"}
+						] },
+						{ "name": "D2", "children": [
+							{ "name": "E1"},
+							{ "name": "E2"}
+						] }
+					]
 				},
 			]
 		  }
@@ -45,58 +73,90 @@ function transformToD3Tree(teams) {
 	return d3.hierarchy(obj)
 }
 
-function buildBracket(teams) {
-	const STYLE = {
-		radius: 200
+class RadialBracket {
+	constructor(teams) {
+		// Convert our team data into a hierachy understandable by d3
+		this.rootNode = transformToD3Tree(teams);
+		this.STYLE = {
+			RADIUS: 300,
+			DOM_ID: '#bracket'
+		};
 	}
 
-	const root = transformToD3Tree(teams);
-	root.sum(function(d) {
-		return d.children && d.children.length > 1 ? 0 : 1;
-	});
-	  
+	build() {
+		this.buildParitionLayout();
+		this.appendSvg();
+		this.addArcs();
+		// this.addText();
+	}
 
-	const { radius } = STYLE;
+	buildParitionLayout() {
+		const rootNode = this.rootNode;
+		// Make only the "leaves" of the tree have nodes
+		rootNode.sum(d => (d.children && d.children.length > 1) ? 0 : 1);
+		const { RADIUS } = this.STYLE;
+		
+		this.partition = d3.partition()
+			.size([2 * Math.PI, RADIUS]);
 	
-	const partition = d3.partition()
-		.size([2 * Math.PI, STYLE.radius]);
+		this.partition(rootNode);
+	}
 
-	partition(root);
+	getArcGenerator() {
+		return d3.arc()
+			.startAngle(d => d.x0)
+			.endAngle(d => d.x1)
+			.innerRadius(d => d.y0)
+			.outerRadius(d => d.y1);
+	}
 
-	const arcGenerator = d3.arc()
-		.startAngle(function(d) { return d.x0; })
-		.endAngle(function(d) { return d.x1; })
-		.innerRadius(function(d) { return d.y0; })
-		.outerRadius(function(d) { return d.y1; });
+	appendSvg() {
+		const { RADIUS, DOM_ID } = this.STYLE;
+		this.svg = d3.select(DOM_ID)
+			.append('svg')
+			  .attr('width', RADIUS*2)
+			  .attr('height', RADIUS*2)
+			.append('g')
+			  .attr("id", "center")
+			  .attr('transform', trans(RADIUS,RADIUS));
+	}
 
-	const svg = d3.select('#bracket')
-		.append('svg')
-		  .attr('width', radius*2)
-		  .attr('height', radius*2)
-		.append('g')
-		  .attr("id", "center")
-		  .attr('transform', trans(radius,radius));
-	
-	d3.select('svg g').selectAll('path')
-		.data(root.descendants())
-		.enter()
-		.append('path')
-		.attr('d', arcGenerator)
-		.attr('class', 'arc')
+	addArcs() {
+		const arcGenerator = this.getArcGenerator();
+		const { rootNode } = this;
 
-	svg.append("g")
-		.attr("pointer-events", "none")
-		.attr("text-anchor", "middle")
-	  .selectAll("text")
-	  .data(root.descendants())
-	  .enter().append("text")
-		.attr("transform", function(d) {
-		  const x = (d.x0 + d.x1) / 2 * 180 / Math.PI;
-		  const y = (d.y0 + d.y1) / 2;
-		  return `rotate(${x - 90}) translate(${y},0) rotate(${x < 180 ? 0 : 180})`;
-		})
-		.attr("dy", "0.35em")
-		.text(d => d.data.name);
+		d3.select('svg g').selectAll('path')
+			.data(rootNode.descendants())
+			.enter()
+			.append('path')
+			.attr('d', arcGenerator)
+			.attr('class', d => {
+				const colorRow = Math.log2(d.value) % 2 == 0;
+				const color = colorRow ? 'light-gray' : 'dark-gray';
+				return color + ' round-arc';
+			})
+	}
+
+	addText() {
+		const { svg, rootNode } = this;
+
+		svg.append("g")
+			.attr("pointer-events", "none")
+			.attr("text-anchor", "middle")
+			.selectAll("text")
+			.data(rootNode.descendants())
+			.enter().append("text")
+			.attr("transform", function(d) {
+				// const x = (d.x0 + d.x1) / 2 * 180 / Math.PI;
+				const y = (d.y0 + d.y1) / 2;
+				// return `rotate(${x - 90}) translate(${y},0) rotate(${x < 180 ? 0 : 180})`;
+				return trans(d.x1, d.y0);
+			})
+			.attr("dy", "0.35em")
+			.text(d => d.data.name);
+	}
 }
 
-buildBracket();
+
+const bracket = new RadialBracket();
+bracket.build();
